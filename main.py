2f3d97f92730d2d799a32cc4aa8c52f73d13b07f
ricardo frança soares <rickfsoaresdev@gmail.com>
2026-03-30 20:13:49 -0300

    refactor: main.py

--- main.py
+++ main.py
@@ -1,22 +1,62 @@
 import yt_dlp
 import subprocess
+import sys
 
-ydl_opts = {
-    "quiet": True,
-    "format": 'bestvideo+bestaudio/best'
-}
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    user_input = input("Search: ")
-    info_data = ydl.extract_info(f"ytsearch5:{user_input}", download=False)
 
-    results = info_data["entries"]
-    url_videos = {}
+def check_user_command(user_input):
+    USER_COMMANDS = ["q", "b"]
+    if user_input.lower() in USER_COMMANDS:
+        return True
 
-    for index, result in enumerate(results):
-        print(f"Index: {index} - Title: {result['title']}")
-        url_videos[str(index)] = result['webpage_url']
+    return False
 
-    user_choice = input("Choice one of the options above(Ex: 1): ")
+
+def user_input_manager(user_input):
+    USER_COMMANDS = {"q": sys.exit, "b": search_video}
+    if user_input.lower() in USER_COMMANDS:
+        USER_COMMANDS[user_input]()
+
+
+def choice_video_option(url_videos):
+    while True:
+        user_choice = input("Choice one of the options above(Ex: 1): ")
+        if check_user_command(user_choice):
+            user_input_manager(user_choice)
+        if user_choice in url_videos:
+            execute_video(user_choice, url_videos)
+            break
+        else:
+            print("Invalid Command")
+
+
+def execute_video(user_choice, url_videos):
     command = ["mpv", url_videos[user_choice]]
-    subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(command, stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
 
+
+def search_video():
+    ydl_opts = {
+        "quiet": True,
+        "format": 'bestvideo+bestaudio/best'
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        user_input = input("Search: ")
+        if check_user_command(user_input):
+            user_input_manager(user_input)
+        info_data = ydl.extract_info(f"ytsearch5:{user_input}", download=False)
+
+        results = info_data["entries"]
+        url_videos = {}
+
+        for index, result in enumerate(results):
+            print(f"Index: {index} - Title: {result['title']}")
+            url_videos[str(index)] = result['webpage_url']
+
+    return url_videos
+
+
+while True:
+    url_videos = search_video()
+    choice_video_option(url_videos)
